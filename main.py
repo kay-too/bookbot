08bc028686bf36f2f638a640ef0bc55586f4af5a
kay-too <mathaiskoulloupias@gmail.com>
2024-10-29 23:03:20 +0000

    Added report function

--- main.py
+++ main.py
@@ -3,18 +3,31 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = count_characters(text)
-    print(text)
-    print(num_words)
-    print(char_count)
+    char_sorted_list = chars_dict_sort(char_count)
+    print("Beginning Report of books/frankestein.txt")
+    print(f"{num_words} words found in the document")
+    print()
 
+    for item in char_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' charcter was found {item['num']} times")
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+    print("End Report")
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_sort(num_chars_dict):
+    sorted_list = []
+    for chars in num_chars_dict:
+        sorted_list.append({"char": chars, "num": num_chars_dict[chars]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def count_characters(text):
     character_count = {}
@@ -25,6 +38,10 @@ def count_characters(text):
             else:
                 character_count[characters] = 1             
 
-    return dict
+    return character_count
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
